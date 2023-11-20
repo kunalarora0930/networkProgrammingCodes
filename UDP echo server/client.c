243a16875807a368f72950765ce0e0c242025271
Kunal Arora <90236283+kunalarora0930@users.noreply.github.com>
2023-11-20 17:52:05 +0530

    Update client.c

--- UDP echo server/client.c
+++ UDP echo server/client.c
@@ -2,19 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
+#include <unistd.h>
 
-#define PORT 12345
-#define MAX_BUFFER_SIZE 1024
+#define SERVER_IP "127.0.0.1"
+#define PORT 6969
+#define BUFFER_SIZE 1024
 
 int main() {
-    int client_socket;
+    int sockfd;
     struct sockaddr_in server_addr;
-    char buffer[MAX_BUFFER_SIZE];
 
-    // Create a UDP socket
-    if ((client_socket = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+    // Create UDP socket
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
@@ -22,35 +21,33 @@ int main() {
     // Configure server address
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
     server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    printf("Enter a message to send to the server: ");
-    fgets(buffer, sizeof(buffer), stdin);
-    buffer[strlen(buffer) - 1] = '\0';  // Remove the newline character
+    char buffer[BUFFER_SIZE];
+    printf("Enter message to send to the server (type 'exit' to quit):\n");
 
-    // Send the user's message to the server
-    if (sendto(client_socket, buffer, strlen(buffer), 0, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        perror("Send error");
-        close(client_socket);
-        exit(EXIT_FAILURE);
+    while (1) {
+        // Read user input
+        fgets(buffer, BUFFER_SIZE, stdin);
+
+        // Send data to the server
+        sendto(sockfd, (const char *)buffer, strlen(buffer), 0, (const struct sockaddr *)&server_addr, sizeof(server_addr));
+
+        if (strcmp(buffer, "exit\n") == 0) {
+            printf("Exiting...\n");
+            break;
+        }
+
+        // Receive the echoed data from the server
+        recvfrom(sockfd, (char *)buffer, BUFFER_SIZE, 0, NULL, NULL);
+
+        // Display the echoed data
+        printf("Server: %s\n", buffer);
     }
 
-    printf("Message sent to server: %s\n", buffer);
-
-    // Receive the echoed message from the server
-    ssize_t received_bytes = recvfrom(client_socket, buffer, sizeof(buffer), 0, NULL, NULL);
-    if (received_bytes == -1) {
-        perror("Receive error");
-        close(client_socket);
-        exit(EXIT_FAILURE);
-    }
-
-    buffer[received_bytes] = '\0';
-    printf("Received message from server: %s\n", buffer);
-
-    // Close the client socket
-    close(client_socket);
+    // Close the socket
+    close(sockfd);
 
     return 0;
 }
